742090d0cbc404dc4ee8ea4f4afffb2511a63bc7
Timothy Merritt <timmybytes@gmail.com>
2021-03-11 09:16:25 -0800

    chore(Homepage): Remove extranneous components

--- pages/index.js
+++ pages/index.js
@@ -27,43 +27,42 @@ export default function Home({ allPostsData }) {
       </Head>
       <Header />
       <main className='container'>
-        <div>
-          <section className='card'>
-            <h2 className='header-2'>
-              Hi! I'm Timothy Merritt, a{' '}
-              <span className='primary'>developer</span>,{' '}
-              <span className='secondary'>designer</span>,{' '}
-              <span className='tertiary-more'>writer</span>, and{' '}
-              <span className='quaternary'>musician</span>*
-            </h2>
-            <p>
-              See more about me in the{' '}
-              <Link href='/about'>
-                <a>About</a>
-              </Link>{' '}
-              page, read my writing about coding and web development in the{' '}
-              <Link href='/blog'>
-                <a>Blog</a>
-              </Link>
-              , or check out some of my featured{' '}
-              <Link href='/work'>
-                <a>Work</a>
-              </Link>
-              .
-            </p>
-            <p>
-              I'm currently looking for new opportunities in frontend roles, so
-              if you'd like to chat about what I can bring to your project,
-              please contact me here!
-            </p>
-            <p>
-              <sub>
-                <sub>* I'm also pretty good at LEGOs.</sub>
-              </sub>
-            </p>
-          </section>
-        </div>
-        <Card />
+        <section
+        // className='card'
+        >
+          <h2 className='header-2'>
+            Hi! I'm Timothy Merritt, a{' '}
+            <span className='primary'>developer</span>,{' '}
+            <span className='secondary'>designer</span>,{' '}
+            <span className='tertiary-more'>writer</span>, and{' '}
+            <span className='quaternary'>musician</span>*
+          </h2>
+          <p>
+            See more about me in the{' '}
+            <Link href='/about'>
+              <a>About</a>
+            </Link>{' '}
+            page, read my writing about coding and web development in the{' '}
+            <Link href='/blog'>
+              <a>Blog</a>
+            </Link>
+            , or check out some of my featured{' '}
+            <Link href='/work'>
+              <a>Work</a>
+            </Link>
+            .
+          </p>
+          <p>
+            I'm currently looking for new opportunities in frontend roles, so if
+            you'd like to chat about what I can bring to your project, please
+            contact me here!
+          </p>
+          <p>
+            <sub>
+              <sub>* I'm also pretty good at LEGOs.</sub>
+            </sub>
+          </p>
+        </section>
       </main>
       <Footer />
     </>
