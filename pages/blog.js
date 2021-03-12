040c9210c6d4bc00e5ae15f933b20daedba3841e
Timothy Merritt <timmybytes@gmail.com>
2021-03-11 17:21:05 -0800

    chore: Refactor blog page with responsive Card components

--- pages/blog.js
+++ pages/blog.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Header from './components/Header/header';
 import Footer from './components/Footer/footer';
+import Card from './components/Card/Card';
 
 export default function Blog({ allPostsData }) {
   return (
@@ -25,49 +26,28 @@ export default function Blog({ allPostsData }) {
       </Head>
       <Header />
       <div className='container'>
-        <h2>Latest Posts</h2>
+        <h2 className='heading'>Latest Posts</h2>
         <main>
           <section className='grid'>
-            <Link href='/posts/keeping-git-commit-messages-consistent-with-a-custom-template'>
-              <a className='card'>
-                <h3>
-                  Keeping Git Commit Messages Consistent with a Custom Template
-                </h3>
-                <img
-                  src='https://res.cloudinary.com/practicaldev/image/fetch/s--137JySGe--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/lhu9sqsut40fqhxcc6xn.png'
-                  alt='colored background'
-                />
-                <p>
-                  Create your own template to easliy guide your commit messages.
-                </p>
-              </a>
-            </Link>
-            <Link href='/posts/what-i-learned-from-100-days-of-code'>
-              <a className='card'>
-                <h3>What I Learned From 100 Days of Code</h3>
-                <img
-                  src='https://res.cloudinary.com/practicaldev/image/fetch/s--uJPlXiA5--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/341pj1urw7drhgbl0qq0.jpeg'
-                  alt='laptop computer'
-                />
-                <p>
-                  Some of what I learned after completing my first
-                  #100DaysOfCode challenge.
-                </p>
-              </a>
-            </Link>
-            <Link href='/posts/adding-ci-cd-to-your-project-with-github-actions'>
-              <a className='card'>
-                <h3>Adding CI/CD to Your Project with GitHub Actions</h3>
-                <img
-                  src='https://res.cloudinary.com/practicaldev/image/fetch/s--137JySGe--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/lhu9sqsut40fqhxcc6xn.png'
-                  alt='colored background'
-                />
-                <p>
-                  Speed up and streamline your workflow with automation and code
-                  checks.
-                </p>
-              </a>
-            </Link>
+            <Card
+              title='Keeping Git Commit Messages Consistent with a Custom Template'
+              body='Create your own template to easliy guide your commit messages.'
+              link='/posts/keeping-git-commit-messages-consistent-with-a-custom-template'
+              date='1/21/2021'
+            />
+            <Card
+              title='What I Learned From 100 Days of Code'
+              body='Some of what I learned after completing my first
+                  #100DaysOfCode challenge.'
+              link='/posts/what-i-learned-from-100-days-of-code'
+              date='10/07/2020'
+            />
+            <Card
+              title='Adding CI/CD to Your Project with GitHub Actions'
+              body='Speed up and streamline your workflow with automation and code checks.'
+              link='/posts/adding-ci-cd-to-your-project-with-github-actions'
+              date='1/22/2021'
+            />
           </section>
         </main>
         <Footer />
--- pages/components/Card/Card.js
+++ pages/components/Card/Card.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import styles from './card.module.scss';
+import Link from 'next/link';
+import { GoCalendar } from 'react-icons/go';
 
-const Card = ({ title, body, date }) => {
+const Card = ({ title, body, date, image, link = '' }) => {
+  const randomImage = () => Math.floor(Math.random() * 3);
   return (
-    <article className={styles.card}>
-      <header className={styles.card__header}>{title || 'Card Title'}</header>
-      <div className={styles.card__body}>
-        <p>
-          {body ||
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, rem a facilis laudantium expedita reiciendis!'}
-        </p>
-      </div>
-      <p className={styles.card__byline}>
-        Published <span>{date || <em>unknown</em>}</span> by{' '}
-        <a>Timothy Merritt</a>
-      </p>
-      <button className={styles.card__button}>
-        <a>Read more!</a>
-      </button>
-    </article>
+    <Link href={link}>
+      <a className={styles.link}>
+        <article className={styles.card}>
+          <img
+            className={styles.card__img}
+            src={
+              image ||
+              `/images/gradients/gradienta-unsplash-${randomImage()}.jpg`
+            }
+          />
+          <header className={styles.card__header}>
+            {title || 'Card Title'}
+          </header>
+          <div className={styles.card__body}>
+            <p>
+              {body ||
+                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, rem a facilis laudantium expedita reiciendis!'}
+            </p>
+          </div>
+          <p className={styles.card__byline}>
+            Published: <GoCalendar />
+            <span> {date || <em>?/?/?</em>}</span>
+          </p>
+        </article>
+      </a>
+    </Link>
   );
 };
 
