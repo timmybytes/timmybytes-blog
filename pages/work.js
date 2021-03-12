ea37c7360370ce64bfd902391eec08275479bbf3
Timothy Merritt <timmybytes@gmail.com>
2021-03-11 17:31:21 -0800

    chore: Update Work projects with BasicCard component

--- pages/work.js
+++ pages/work.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Header from './components/Header/header';
 import Footer from './components/Footer/footer';
+import { BasicCard } from './components/Card/Card';
 
 export default function Work({ allPostsData }) {
   return (
@@ -25,47 +26,29 @@ export default function Work({ allPostsData }) {
       </Head>
       <Header />
       <div className='container'>
-        <h2>Work</h2>
+        <h2 className='heading'>Work</h2>
         <main>
           <section className='grid'>
-            <Link href='https://timmybytes.github.io/rational-design/'>
-              <a className='card'>
-                <h3 style={{ fontSize: '1.5rem' }}>Rational Design</h3>
-                <img
-                  src='https://raw.githubusercontent.com/timmybytes/rational-design/a3bd89b918b051840fb8301c5c5cf9b9404f36fa/src/assets/rational-design-logo.svg'
-                  alt='Rational Design logo'
-                />
-                <p>
-                  A tool for creating ratio-based style systems built in React.{' '}
-                </p>
-              </a>
-            </Link>
-            <Link href='https://timmybytes.github.io/resolute-apparel'>
-              <a className='card'>
-                <h3 style={{ fontSize: '1.5rem' }}>Resolute Apparel</h3>
-                <img
-                  src='https://raw.githubusercontent.com/timmybytes/resolute-apparel/dev/src/assets/resolute-apparel-social-card.png'
-                  alt='Resolute Apparel logo'
-                />
-                <p>
-                  A full-stack e-commerce site built from React, React-Router,
-                  Redux, SCSS, Firebase, and Stripe.{' '}
-                </p>
-              </a>
-            </Link>
-            <Link href='https://github.com/timmybytes/dotfiles'>
-              <a className='card'>
-                <h3 style={{ fontSize: '1.5rem' }}>Dotfiles</h3>
-                <img
-                  src='https://raw.githubusercontent.com/timmybytes/dotfiles/main/dotfiles-logo.svg'
-                  alt='dotfiles logo'
-                />
-                <p>
-                  My personal (.)dotfiles and configurations for various
-                  programs and environments.{' '}
-                </p>
-              </a>
-            </Link>
+            <BasicCard
+              title='Rational Design'
+              body='A tool for creating ratio-based style systems built in React.'
+              link='https://github.com/timmybytes/rational-design'
+              image='https://raw.githubusercontent.com/timmybytes/rational-design/a3bd89b918b051840fb8301c5c5cf9b9404f36fa/src/assets/rational-design-logo.svg'
+            />
+            <BasicCard
+              title='Resolute Apparel'
+              body='A full-stack e-commerce site built from React,
+                  Redux, SCSS, Firebase, and Stripe.'
+              link='https://github.com/timmybytes/resolute-apparel'
+              image='https://raw.githubusercontent.com/timmybytes/resolute-apparel/dev/src/assets/resolute-apparel-social-card.png'
+            />
+            <BasicCard
+              title='Dotfiles'
+              body='My personal (.)dotfiles and configurations for various
+                  programs and environments.'
+              image='https://raw.githubusercontent.com/timmybytes/dotfiles/main/dotfiles-logo.svg'
+              link='https://github.com/timmybytes/dotfiles'
+            />
           </section>
         </main>
         <Footer />
