2d80c75643220b14f1bebdd05b1d7b75e7d5b4dc
Timothy Merritt <timmybytes@gmail.com>
2021-03-14 09:38:43 -0700

    chore(Head): Consolidate <head> metadata in _document.js

--- pages/about.js
+++ pages/about.js
@@ -8,34 +8,6 @@ import { useAppContext } from '../contexts/AppContext';
 const About = () => {
   return (
     <>
-      <Head>
-        <title>timmybytes</title>
-        <meta name='title' content='timmybytes' />
-        <meta
-          name='description'
-          content="Timothy Merritt's developer portfolio and blog"
-        />
-        <link rel='icon' href='/favicon.ico' />
-        {/* Open graph */}
-        <meta property='og:title' content='timmybytes' />
-        <meta
-          property='og:description'
-          content="Timothy Merritt's development portfolio and blog"
-        />
-        <meta property='og:url' content='https://timmybytes.com' />
-        <meta property='og:type' content='website' />
-        <meta property='og:image' content='../public/images/SocialCard.png' />
-        <meta property='og:image:type' content='image/png' />
-        <meta property='og:image:width' content='1200' />
-        <meta property='og:image:height' content='647' />
-        <meta property='og:image:alt' content='The timmybytes logo' />
-        <meta property='twitter:card' content='summary_large_image' />
-        <meta
-          property='twitter:image'
-          content='../public/images/SocialCard.png'
-        />
-        <meta property='twitter:creator' content='@timmybytes' />
-      </Head>
       <Header />
       <main className='container-gen'>
         <img
