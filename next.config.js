930afdf5102198a4de813aa6fcb0b09aeb498d5d
Timothy Merritt <timmybytes@gmail.com>
2021-03-24 16:07:56 -0700

    build: Fix webpack module build errors --WIP

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     // !! WARN !!
-    ignoreBuildErrors: true,
+    // ignoreBuildErrors: true,
   },
   i18n: {
     locales: ['en-US'],
