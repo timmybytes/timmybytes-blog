bcf4211c95199bb1b2e384d4ee3368d078dba7ab
Timothy Merritt <timmybytes@gmail.com>
2021-03-25 11:59:42 -0700

    chore(favicon): Add cross-device/browser favicons

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,28 @@ class MyDocument extends Document {
             name='description'
             content="Timothy Merritt's developer portfolio and blog."
           />
-          <link rel='icon' href='/favicon.ico' />
+          {/* Icons and Favicons */}
+          <link
+            rel='apple-touch-icon'
+            sizes='180x180'
+            href='/apple-touch-icon.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+            href='/favicon-32x32.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+            href='/favicon-16x16.png'
+          />
+          <link rel='manifest' href='/site.webmanifest' />
+          <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+          <meta name='msapplication-TileColor' content='#da532c' />
+          <meta name='theme-color' content='#ff90ad' />
           {/* Open graph */}
           <meta property='og:locale' content='en_US' />
           <meta property='og:type' content='website' />
