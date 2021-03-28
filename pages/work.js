9c1feb4e4623b0f7a0e404d29ebd3b6775358632
Timothy Merritt <timmybytes@gmail.com>
2021-03-27 17:15:59 -0700

    style(work): Add layout structure to work page

--- pages/work.js
+++ pages/work.js
@@ -1,32 +1,41 @@
 import BasicCard from '../components/Cards/BasicCard';
+import Head from 'next/head';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
 
 export default function Work({ allPostsData }) {
   return (
     <>
-      <a className='anchor' id='work'></a>
-      <h2 className='heading'>Work</h2>
-      <section className='grid'>
-        <BasicCard
-          title='Rational Design'
-          body='A tool for creating ratio-based style systems built in React.'
-          link='https://github.com/timmybytes/rational-design'
-          image='https://raw.githubusercontent.com/timmybytes/rational-design/a3bd89b918b051840fb8301c5c5cf9b9404f36fa/src/assets/rational-design-logo.svg'
-        />
-        <BasicCard
-          title='Resolute Apparel'
-          body='A full-stack e-commerce site built from React,
+      <Head>
+        <title>timmybytes</title>
+      </Head>
+      <Header />
+      <main className='container'>
+        <h2 className='heading center'>Work</h2>
+        <section className='grid'>
+          <BasicCard
+            title='Rational Design'
+            body='A tool for creating ratio-based style systems built in React.'
+            link='https://github.com/timmybytes/rational-design'
+            image='https://raw.githubusercontent.com/timmybytes/rational-design/a3bd89b918b051840fb8301c5c5cf9b9404f36fa/src/assets/rational-design-logo.svg'
+          />
+          <BasicCard
+            title='Resolute Apparel'
+            body='A full-stack e-commerce site built from React,
                   Redux, SCSS, Firebase, and Stripe.'
-          link='https://github.com/timmybytes/resolute-apparel'
-          image='https://raw.githubusercontent.com/timmybytes/resolute-apparel/dev/src/assets/resolute-apparel-social-card.png'
-        />
-        <BasicCard
-          title='Dotfiles'
-          body='My personal (.)dotfiles and configurations for various
+            link='https://github.com/timmybytes/resolute-apparel'
+            image='https://raw.githubusercontent.com/timmybytes/resolute-apparel/dev/src/assets/resolute-apparel-social-card.png'
+          />
+          <BasicCard
+            title='Dotfiles'
+            body='My personal (.)dotfiles and configurations for various
                   programs and environments.'
-          image='https://raw.githubusercontent.com/timmybytes/dotfiles/main/dotfiles-logo.svg'
-          link='https://github.com/timmybytes/dotfiles'
-        />
-      </section>
+            image='https://raw.githubusercontent.com/timmybytes/dotfiles/main/dotfiles-logo.svg'
+            link='https://github.com/timmybytes/dotfiles'
+          />
+        </section>
+      </main>
+      <Footer />
     </>
   );
 }
