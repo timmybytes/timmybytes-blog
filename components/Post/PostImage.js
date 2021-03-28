d1ae382434d8548936010ecd6052f889df7f0ebb
Timothy Merritt <timmybytes@gmail.com>
2021-03-27 17:12:55 -0700

    style(PostImage): Add shadow transition to post images

--- components/Post/PostImage.js
+++ components/Post/PostImage.js
@@ -9,11 +9,17 @@ const Image = styled.img`
   width: 100%;
   height: auto;
   margin: 1rem auto;
-  transition: 0.3s;
-  &:hover {
-    transform: translate(-4px, -4px);
-    box-shadow: 4px 4px 0 0 ${colors.colorDark};
-  }
+  transition: 0.9s;
+  transform: translate(-8px, -8px);
+  box-shadow: 0.5px 0.5px 0 0 ${colors.colorDark},
+    1px 1px 0 0 ${colors.colorDark}, 1.5px 1.5px 0 0 ${colors.colorDark},
+    2px 2px 0 0 ${colors.colorDark}, 2.5px 2.5px 0 0 ${colors.colorDark},
+    3px 3px 0 0 ${colors.colorDark}, 3.5px 3.5px 0 0 ${colors.colorDark},
+    4px 4px 0 0 ${colors.colorDark}, 4.5px 4.5px 0 0 ${colors.colorDark},
+    5px 5px 0 0 ${colors.colorDark}, 5.5px 5.5px 0 0 ${colors.colorDark},
+    6px 6px 0 0 ${colors.colorDark}, 6.5px 6.5px 0 0 ${colors.colorDark},
+    7px 7px 0 0 ${colors.colorDark}, 7.5px 7.5px 0 0 ${colors.colorDark},
+    8px 8px 0px ${colors.colorDark};
 `;
 
 const PostImage = ({ src }) => {
