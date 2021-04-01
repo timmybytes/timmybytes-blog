f5c8fb369cc1d993a4592d127d97e3444968ee66
Timothy Merritt <timmybytes@gmail.com>
2021-04-01 06:55:53 -0700

    chore(Intro): Add social media links

--- components/Intro/Intro.js
+++ components/Intro/Intro.js
@@ -1,10 +1,51 @@
 import styled from 'styled-components';
+import {
+  SiKoFi,
+  SiLinkedin,
+  SiDevDotTo,
+  SiTwitter,
+  SiDribbble,
+  SiGithub,
+} from 'react-icons/si';
 import React, { useState } from 'react';
 import Highlight from '../Highlight/Highlight';
 import styles from './intro.module.scss';
 import About from '../Intro/About/About';
 import { colors } from '../utils';
 
+const SocialLinks = styled.ul`
+  align-items: center;
+  display: flex;
+  justify-items: stretch;
+  list-style: inside none;
+  margin: 1rem auto;
+  padding: 1rem 0;
+  li {
+    & a:hover {
+      border: none;
+    }
+    & a:visited {
+      color: none;
+    }
+    svg {
+      border-radius: 4px;
+      color: ${colors.dark};
+      height: auto;
+      margin: 1rem;
+      padding: 7px 10px;
+      transition: 0.3s;
+      width: 38px;
+      &:hover {
+        box-shadow: 4px 4px 0 0 ${colors.dark};
+        transform: translate(-4px, -4px);
+      }
+      @media screen and (max-width: 400px) {
+        margin: 0.25rem;
+        padding: 2px 5px;
+      }
+    }
+  }
+`;
 const IntroCard = () => {
   const [isClicked, setIsClicked] = useState(false);
   const handleClick = () => {
@@ -38,6 +79,38 @@ const IntroCard = () => {
           </button>
         </About>
       )}
+      <SocialLinks>
+        <li>
+          <a href='https://twitter.com/timmybytes' target='_blank'>
+            <SiTwitter style={{ background: colors.primary }} />
+          </a>
+        </li>
+        <li>
+          <a href='https://github.com/timmybytes' target='_blank'>
+            <SiGithub style={{ background: colors.secondary }} />
+          </a>
+        </li>
+        <li>
+          <a href='https://www.linkedin.com/in/timmybytes' target='_blank'>
+            <SiLinkedin style={{ background: colors.tertiary }} />
+          </a>
+        </li>
+        <li>
+          <a href='https://dev.to/timmybytes' target='_blank'>
+            <SiDevDotTo style={{ background: colors.quaternary }} />
+          </a>
+        </li>
+        <li>
+          <a href='https://dribbble.com/timmybytes' target='_blank'>
+            <SiDribbble style={{ background: colors.primary }} />
+          </a>
+        </li>
+        <li>
+          <a href='https://ko-fi.com/O4O82TAZB' target='_blank'>
+            <SiKoFi style={{ background: colors.secondary }} />
+          </a>
+        </li>
+      </SocialLinks>
     </section>
   );
 };
--- next.config.js
+++ next.config.js
@@ -10,7 +10,11 @@ module.exports = {
     locales: ['en-US'],
     defaultLocale: 'en-US',
   },
-  future: {
-    webpack5: true,
-  },
+  // Webpack 5 breaks `npm run dev` via unresolved:
+  //    TypeError [ERR_INVALID_ARG_VALUE]: The argument 'path' must be a string
+  //    or Uint8Array without null bytes.
+  // Does not appear to affect production, however.
+  // future: {
+  //   webpack5: true,
+  // },
 };
