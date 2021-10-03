639567ac2f4ad0c0c57e7f025ddf4904b67bf714
Timothy Merritt <timmybytes@gmail.com>
2021-10-02 17:32:16 -0700

    chore: Add local project images

--- src/components/WorkPreview/WorkPreview.js
+++ src/components/WorkPreview/WorkPreview.js
@@ -1,4 +1,4 @@
-import Card from '@/components/Cards/Card';
+import Card from '@/components/Cards/Card'
 
 export default function WorkPreview({ allPostsData }) {
   return (
@@ -11,25 +11,24 @@ export default function WorkPreview({ allPostsData }) {
           title='Banquet'
           body='A gourmet Next.js starter + boilerplate'
           link='https://github.com/timmybytes/banquet'
-          image='https://raw.githubusercontent.com/timmybytes/banquet/main/public/banquet-card.png'
+          image='/images/banquet-card.svg'
           alt='Banquet Logo'
         />
         <Card
           title='Rational Design'
           body='A tool for creating ratio-based style systems built in React.'
           link='https://github.com/timmybytes/rational-design'
-          image='https://raw.githubusercontent.com/timmybytes/rational-design/a3bd89b918b051840fb8301c5c5cf9b9404f36fa/src/assets/rational-design-logo.svg'
+          image='/images/rational-design-logo.svg'
           alt='Rational Design logo'
         />
         <Card
           title='Dotfiles'
           body='My personal (.)dotfiles and configurations for various
                   programs and environments.'
-          image='https://raw.githubusercontent.com/timmybytes/dotfiles/main/dotfiles-logo.svg'
-          link='https://github.com/timmybytes/dotfiles'
+          image='/images/dotfiles-logo.svg'
           alt='Dotfiles logo'
         />
       </section>
     </>
-  );
+  )
 }
