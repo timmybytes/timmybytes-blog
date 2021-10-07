9082c44cc8c438896cd0ce9bcfef4efa44496503
Timothy Merritt <timmybytes@gmail.com>
2021-10-06 20:03:09 -0700

    test: Added required test links

--- src/components/SideNav/SideNav.test.tsx
+++ src/components/SideNav/SideNav.test.tsx
@@ -1,8 +1,14 @@
 import { render } from '@test/test-utils'
 import { SideNav } from './SideNav'
 
+const HOMEPAGE_LINKS = [
+  { url: '#about', name: 'About' },
+  { url: '#projects', name: 'Projects' },
+  { url: '#contact', name: 'contact' },
+]
+
 describe('SideNav', () => {
   it('should render', () => {
-    render(<SideNav />)
+    render(<SideNav links={HOMEPAGE_LINKS} />)
   })
 })
